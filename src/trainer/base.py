1a023f9b4e80b4ed2360ad5a0de8d7680283683d
Filip Sondej <26285707+filyp@users.noreply.github.com>
2026-01-29 10:54:49 +0100

    . cleaner trainer/base.py

--- src/trainer/base.py
+++ src/trainer/base.py
@@ -26,32 +26,29 @@ class FinetuneTrainer(Trainer):
         trial: Dict[str, Any] = None,
     ) -> Dict[str, float]:
         # Run a custom evaluator and save results
-        if self.evaluators:
-            if self.accelerator.is_local_main_process:
-                eval_metrics = {}
-                if self.accelerator.num_processes == 1:
-                    run_dir = self._get_output_dir(trial=trial)
-                    checkpoint_folder = (
-                        f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
-                    )
-                    output_dir = os.path.join(run_dir, checkpoint_folder, "evals")
-                    os.makedirs(output_dir, exist_ok=True)
-                    eval_metrics = {}
-                    for _, evaluator in self.evaluators.items():
-                        eval_args = {
-                            "output_dir": output_dir,
-                            "template_args": self.template_args,
-                            "model": self.model,
-                            "tokenizer": self.processing_class,
-                            "trainer": self,
-                        }
-                        eval_metrics.update(evaluator.evaluate(**eval_args))
-                    self.log(eval_metrics)
-                else:
-                    logger.warning(
-                        "Custom evaluator can be run with this Trainer only when a single accelerator process is running."
-                    )
-                return eval_metrics
+        if self.evaluators and self.accelerator.is_local_main_process:
+            if self.accelerator.num_processes != 1:
+                logger.warning(
+                    "Custom evaluator can be run with this Trainer only when a single accelerator process is running."
+                )
+                return {}
+
+            run_dir = self._get_output_dir(trial=trial)
+            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
+            output_dir = os.path.join(run_dir, checkpoint_folder, "evals")
+            os.makedirs(output_dir, exist_ok=True)
+            eval_metrics = {}
+            for _, evaluator in self.evaluators.items():
+                eval_args = {
+                    "output_dir": output_dir,
+                    "template_args": self.template_args,
+                    "model": self.model,
+                    "tokenizer": self.processing_class,
+                    "trainer": self,
+                }
+                eval_metrics.update(evaluator.evaluate(**eval_args))
+            self.log(eval_metrics)
+            return eval_metrics
 
         if eval_dataset is None or eval_dataset == "dummy":
             return {}
